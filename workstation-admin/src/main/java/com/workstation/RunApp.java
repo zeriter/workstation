385d18770e983324d173d70a9977faedc3768f28
zeriter zhang <791825185@qq.com>
2023-12-02 18:23:18 +0800

    构建项目基础框架

---
+++ workstation-admin/src/main/java/com/workstation/RunApp.java
@@ -0,0 +1,17 @@
+package com.workstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZERITER-ZHANG
+ * @version 0.0.1
+ * @description 启动类
+ * @date 2023/12/2 16:56 周六
+ */
+@SpringBootApplication
+public class RunApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RunApp.class, args);
+    }
+}
